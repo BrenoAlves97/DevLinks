56be0b5a1bdaa152f8caffb729ad6a80b09dac01
Breno Alves <breno_ja@hotmail.com>
2022-12-25 14:06:42 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+//Selecionando o elemento
+const htmlRoot = document.querySelector("html")
+const switchMode = document.querySelector("#switch button")
+const imageChange = document.querySelector("#profile img")
+
+//Alterando o modo de tela;
+switchMode.addEventListener("click", function () {
+  //Maneira de realizar
+  setTimeout(() => {
+    htmlRoot.classList.toggle("light")
+    changeImage()
+  }, 250)
+})
+
+function changeImage() {
+  const isLightMode = htmlRoot.classList.contains("light")
+  if (isLightMode) {
+    //alterando imagem se tiver no modo light
+    imageChange.setAttribute("src", "../assets/profile-with-glasses.png")
+    imageChange.setAttribute("alt", "Foto de Breno Alves de óculos")
+  } else {
+    //mantendo imagem normal
+    imageChange.setAttribute("src", "../assets/profile.png")
+    imageChange.setAttribute("alt", "Foto de Breno Alves")
+  }
+}
